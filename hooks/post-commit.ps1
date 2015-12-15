f55bd94c07898ade3e2f9247a95d98f5655f7b70
pesetskyps <pavel_pesetskiy@epam.com>
2015-12-15 15:02:29 +0300

    move hooks to powershell

---
+++ hooks/post-commit.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference="Stop"
+$ScriptDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$i = 0
+$WorkDirectory = $ScriptDirectory.Split('\')
+$comp = $WorkDirectory.IndexOf(".git") - 1
+$GitDirectory = ""
+
+($i..$comp).Foreach{ $GitDirectory += $WorkDirectory[$_]; $GitDirectory += "\"  }
+$RerereLink="$GitDirectory.git\rr-cache"
+
+$RerereLinkResolver = fsutil reparsepoint query $RerereLink | Select-String "Print Name:"
+$RerereLinkResolver -cmatch "([A-Z]:.*)"
+$RerereLinkResolver = $Matches[0]
+#git -C $RerereRootDir add .
+#git -C $RerereRootDir commit -m "$CommitMessage"
+#git -C $RerereRootDir push origin master
