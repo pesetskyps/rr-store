be50fc632723f52221bbfe532d40efb4f44ae9cf
hlebloika <hleb_loika@epam.com>
2015-12-16 11:45:28 +0300

    rerere

---
+++ ClaimKatana/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClaimKatana.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int bla14)
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View((User as ClaimsPrincipal).Claims);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ClaimKatana/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClaimKatana.Startup))]
+namespace ClaimKatana
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
